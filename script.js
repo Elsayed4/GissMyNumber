e7382e2705c9279cfa7a245555bf60985888a9f4
ElsayedTorky <etorky4@gmail.com>
2022-04-15 13:15:56 +0200

    second

--- script.js
+++ script.js
@@ -3,12 +3,12 @@
 
 let check = document.querySelector('.check');
 let guess = document.querySelector('.guess');
-let secretNumber = Math.trunc(Math.random() * 5) + 1;
+let secretNumber = Math.trunc(Math.random() * 10) + 1;
 let numberele = document.querySelector('.number');
 let message = document.querySelector('.message');
 let score = 20;
 let again = document.querySelector('.again');
-console.log(secretNumber); 
+let highScore = 0;
 
 check.addEventListener('click',function(){
     
@@ -39,12 +39,19 @@ if(!guessNumber){
         }
     }else{
         // Player Wins
+    
         document.querySelector('.score').textContent = score;
-numberele.textContent = secretNumber;
+        
+        numberele.textContent = secretNumber;
 
         message.textContent = "Great You Win";
         document.body.style.backgroundColor = '#60b347';
         numberele.style.width = '30rem';
+
+        if(score > highScore){
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
     }
 }
 
@@ -53,8 +60,10 @@ numberele.textContent = secretNumber;
 });
 
 again.addEventListener('click', function(){
+    secretNumber = Math.trunc(Math.random() * 10) + 1
     guess.value = ' ';
     document.querySelector('.score').textContent = 20;
+    score = 20;
     document.body.style.backgroundColor = '#222';
     numberele.textContent = '?';
     message.textContent = "Start guessing...";
